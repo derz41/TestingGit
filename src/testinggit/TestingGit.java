2cdfa8408d55f2d623a8d39e141950aa7900c624
derz41 <andersonm@chc.edu>
2017-01-24 08:58:03 -0500

    Version 2

--- src/testinggit/TestingGit.java
+++ src/testinggit/TestingGit.java
@@ -15,7 +15,9 @@ public class TestingGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Movie carrie = new Movie("carrie", "Stephen King");
+        
     }
     
 }
