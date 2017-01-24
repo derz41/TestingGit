640da5ddc2633f8b0537d8c9138c40d3bfb88954
derz41 <andersonm@chc.edu>
2017-01-24 08:51:59 -0500

    Version 1

---
+++ src/Movie.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matt
+ */
+public class Movie {
+    
+    private String title;
+    private String author;
+    private int numberPages;
+
+    public Movie(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberPages() {
+        return numberPages;
+    }
+
+    public void setNumberPages(int numberPages) {
+        this.numberPages = numberPages;
+    }
+    
+    
+    
+}
